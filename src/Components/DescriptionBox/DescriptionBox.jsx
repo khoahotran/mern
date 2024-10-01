96520c246b5fefdd68c4cee6a1094427574b4da7
khoahotran <trannguyenanhkhoa0104@gmail.com>
2024-10-01 23:16:32 +0700

    done basic UI and next is respoonsive at 3h0p

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './DescriptionBox.css'
+
 const DescriptionBox = () => {
   return (
     <div className='descriptionbox'>
-      
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>An e-commerce website is online platform...</p>
+        <p>E-commerce websites typically display product ...</p>
+      </div>
+
     </div>
   )
 }
